c14cffd08c01ca5751f12ab410d713917a3c988d
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-02-14 23:15:54 -0500

    isaacgym environment added

---
+++ Go2Py/sim/gym/__init__.py
@@ -0,0 +1,7 @@
+
+
+import os
+
+MINI_GYM_ROOT_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '..')
+print(MINI_GYM_ROOT_DIR)
+MINI_GYM_ENVS_DIR = os.path.join(MINI_GYM_ROOT_DIR, 'go_gym', 'envs')
