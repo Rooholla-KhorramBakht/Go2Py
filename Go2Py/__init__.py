f2ee4f152678a0224ec5fa5031a6586a02fe62b1
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-02-20 22:27:08 -0500

    Isaacsim and Mujoco simulator classes are added.

---
+++ Go2Py/__init__.py
@@ -0,0 +1,9 @@
+import os
+import platform
+import sys
+
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
+GO2_USD_PATH = os.path.join(os.path.dirname(__file__), "assets/usd/go2.usd")
+GO2_ISAACSIM_CFG_PATH = os.path.join(
+    os.path.dirname(__file__), "sim/isaacsim/sim_config.yaml"
+)
