5747142ac08732544004cb6cdfcf158a8426e7a2
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-02-23 02:10:56 +0800

    new map added and navigation cmd_vel remapped to /go2/twist_cmd

--- deploy/nav2_ws/src/sportmode_nav2/launch/navigation.launch.py
+++ deploy/nav2_ws/src/sportmode_nav2/launch/navigation.launch.py
@@ -47,7 +47,8 @@ def generate_launch_description():
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', '/tf'),
-                  ('/tf_static', '/tf_static')]
+                  ('/tf_static', '/tf_static'),
+                  ('/cmd_vel', '/go2/twist_cmd')]
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
