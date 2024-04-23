5b5eb831faa8c803b9d605a41afc0d87e4ff4323
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-04-24 06:51:05 +0800

    nav2 bugs fixed

--- nav2_docker_run.sh
+++ nav2_docker_run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --privileged --network host -v $(pwd)/deploy/nav2_ws:/home/nav2_ws -v /dev/*:/dev/* -v /etc/localtime:/etc/localtime:ro --runtime nvidia go2py_nav:latest
+docker run --rm -it --privileged --network host -v $(pwd)/deploy/nav2_ws/src:/home/nav2_ws/src -v /dev/*:/dev/* -v /etc/localtime:/etc/localtime:ro --runtime nvidia go2py_nav:latest
