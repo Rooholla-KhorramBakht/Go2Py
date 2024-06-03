2590b1c9148e4d6f6a7da934b182d7aadc5408d6
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-06-02 22:53:18 -0400

    Cyclonedds configs are added to the startup scripts

--- deploy/docker/scripts/bridge_start.sh
+++ deploy/docker/scripts/bridge_start.sh
@@ -1,4 +1,7 @@
 source /opt/ros/humble/setup.bash
 source /unitree_ros2/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+                            <NetworkInterface name="eth0" priority="default" multicast="default" />
+                        </Interfaces></General></Domain></CycloneDDS>'
 source /bridge_ws/install/setup.bash && ros2 launch /root/launch/bridge.launch.py
--- deploy/docker/scripts/hesai_start.sh
+++ deploy/docker/scripts/hesai_start.sh
@@ -1,4 +1,7 @@
 source /opt/ros/humble/setup.bash
 source /unitree_ros2/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+                            <NetworkInterface name="eth0" priority="default" multicast="default" />
+                        </Interfaces></General></Domain></CycloneDDS>'
 source /hesai_ws/install/setup.bash && ros2 launch /root/launch/hesai.launch.py
